33382d6692c9016dfa3bfacbff7d894d80c4835b
HJHSUO <hjhsuo@HJHSUOdeMacBook-Air.local>
2018-12-07 22:29:56 +0800

    update times

--- guessnumber.py
+++ guessnumber.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count = count + 1
 	n = input('1~100,猜一個數字:')
 	n = int(n)
 	if n == r:
@@ -8,6 +10,8 @@ while True:
 		break
 	elif n > r:
 		print('猜得比答案大')
+		print('這是你猜的第',count,'次')
 	elif n < r:
 		print('猜得比答案小')
+		print('這是你猜的第',count,'次')
 
